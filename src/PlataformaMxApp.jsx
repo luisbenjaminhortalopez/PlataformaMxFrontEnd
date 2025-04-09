4ab61e9f7a3cc7e6e293fa0f9710b70120e1302c
Rodrigo <rodrigo17.ed@gmail.com>
2025-04-08 20:35:51 -0600

    Configuración tailwind

--- src/PlataformaMxApp.jsx
+++ src/PlataformaMxApp.jsx
@@ -1,8 +1,12 @@
+import { createBrowserRouter, RouterProvider } from 'react-router'
+import { AppRouter } from './router'
+import './style.css';
+
+
+const router = createBrowserRouter( AppRouter );
 
 export const PlataformaMxApp = () => {
     return (
-        <>
-            Rama 
-        </>
+        <RouterProvider router = { router } />
     );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router'
-import { AppRouter } from './router'
+import { PlataformaMxApp } from './PlataformaMxApp';
 
-const router = createBrowserRouter( AppRouter );
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router = { router } />
+    <PlataformaMxApp />
   </StrictMode>,
 )
