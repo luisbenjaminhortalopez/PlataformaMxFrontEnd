6cd66506ae7562d360c8188ac73ce2d13b0b4593
Cesar Trujillo <rayzler.404@gmail.com>
2025-07-05 12:06:53 -0600

    Login page

---
+++ src/app/login/page.tsx
@@ -0,0 +1,29 @@
+import Link from "next/link";
+import { Image } from "@home/components/image";
+import { LoginForm } from "@admin/components/loginForm";
+
+const LoginPage = () => {
+  return (
+    <div className="min-h-screen bg-white flex items-center justify-center">
+      <div className="bg-black rounded-lg px-10 py-8 w-full max-w-md shadow-md text-white text-center">
+        <Image
+          src="/Logo.svg"
+          alt="PlataformaMX"
+          className="h-10 mx-auto mb-4"
+        />
+        <h2 className="text-2xl font-bold mb-2">Administrador</h2>
+
+        <LoginForm />
+
+        <Link
+          href="/"
+          className="text-sm text-gray-400 mt-4 inline-block hover:underline"
+        >
+          Ir a PlataformaMX
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default LoginPage;
---
+++ src/modules/admin/components/loginForm.tsx
@@ -0,0 +1,68 @@
+"use client";
+
+import { login } from "@/actions/auth";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+
+export const LoginForm = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const router = useRouter();
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setError("");
+
+    try {
+      const response = await login(username, password);
+      const { usuario } = response.data;
+
+      localStorage.setItem("isAuthenticated", "true");
+      localStorage.setItem("adminId", usuario.id);
+
+      router.push("/admin/noticias");
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    } catch (err) {
+      setError("Credenciales inválidas o error del servidor.");
+    }
+  };
+
+  return (
+    <form
+      onSubmit={handleSubmit}
+      className="flex flex-col gap-4 text-left mt-4"
+    >
+      <div>
+        <label className="block mb-1">Usuario</label>
+        <input
+          type="text"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          className="w-full px-4 py-2 rounded bg-white text-black outline-none"
+          required
+        />
+      </div>
+
+      <div>
+        <label className="block mb-1">Contraseña</label>
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          className="w-full px-4 py-2 rounded bg-white text-black outline-none"
+          required
+        />
+      </div>
+
+      {error && <p className="text-red-500 text-sm text-center">{error}</p>}
+
+      <button
+        type="submit"
+        className="bg-gray-600 text-white py-2 rounded hover:bg-gray-700 transition"
+      >
+        Entrar
+      </button>
+    </form>
+  );
+};
